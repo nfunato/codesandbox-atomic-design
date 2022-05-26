00bbd756e54be19968c970779b0e2b37f41645b6
Nobuhiko FUNATO <nfunato@acm.org>
2022-05-27 02:28:51 +0900

    add useContext usecase of 1/2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { Router } from "./router/Router";
 import "./styles.css";
-
-const user = {
-  name: "わしじゃ",
-  image: "https://source.unsplash.com/gKXKBY-C-Dk",
-  email: "12345@example.com",
-  phone: "1234-5679-9999",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
+import { UserProvider } from "./providers/UserProvider";
 
 export default function App() {
-  return <Router />;
+  return (
+    <UserProvider>
+      <Router />
+    </UserProvider>
+  );
 }
