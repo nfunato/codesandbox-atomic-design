2682cb688c7cc53c0965dc4e658bfaed5b7d72fe
Nobuhiko FUNATO <nfunato@acm.org>
2022-05-27 00:02:18 +0900

    add page function (wip)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-import "./styles.css";
+import { Router } from "react-router-dom";
 
 const user = {
   name: "わしじゃ",
@@ -20,14 +13,8 @@ const user = {
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
+    <div>
+      <p>ほげ</p>
+    </div>
   );
 }
