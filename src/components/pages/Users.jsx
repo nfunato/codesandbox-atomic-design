7de978900a84288dcf594dc00f4f9c4f89d466a4
Nobuhiko FUNATO <nfunato@acm.org>
2022-05-27 00:49:03 +0900

    wip

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,13 +1,17 @@
 import styled from "styled-components";
-
+import { SearchInput } from "../molecules/SearchInput";
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザ一覧</h2>
+      <SearchInput />
     </SContainer>
   );
 };
 
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
 `;
